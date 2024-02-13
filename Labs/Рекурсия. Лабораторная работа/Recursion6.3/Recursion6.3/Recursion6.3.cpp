27be8fb263ef37412edbb6fd4e5dcbe1829a405c
Andr0medA007 <98257828+Andr0medA007@users.noreply.github.com>
2024-02-13 19:55:53 +0500

    Лабораторные работы

---
+++ Labs/Рекурсия. Лабораторная работа/Recursion6.3/Recursion6.3/Recursion6.3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+static int Hanoisk(int n, int start, int tmp, int final) {
+    if (n > 0) {
+        Hanoisk(n - 1, start, tmp, final);
+        cout << start << " =>" << tmp << endl;
+        Hanoisk(n - 1, tmp, final,start);
+    }
+    return 0;
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    int n;
+    cout << "Введите количество колец на башне: " << endl;
+    cin >> n;
+    Hanoisk(n, 1, 2, 3);
+    return 0;
+}
