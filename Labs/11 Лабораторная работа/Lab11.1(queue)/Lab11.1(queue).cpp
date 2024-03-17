b713ef1e0fae70aa36fe5117bb5f59253252095f
Andr0medA007 <98257828+Andr0medA007@users.noreply.github.com>
2024-03-17 11:17:49 +0500

    11 и 8 лаб. работы

---
+++ Labs/11 Лабораторная работа/Lab11.1(queue)/Lab11.1(queue).cpp
@@ -0,0 +1,121 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Node {
+    char data;
+    Node* next;
+};
+struct queue {
+    int size;
+    Node* hn;
+    Node* tn;
+};
+
+void init_queue(queue& q, const char& value);
+void new_queue(queue& q, int n);
+void push(queue& q, const char& value);
+void print(queue& q);
+void pop(queue& q);
+void del_key(queue& q, char r);
+void insert(queue& q,int count, char r);
+
+int main()
+{
+    system("chcp 1251");
+    queue q;
+    int n;
+    char r;
+    do {
+        cout << "Введите длину очереди: " << endl;
+        cin >> n;
+    } while (n <= 0);
+    new_queue(q, n);
+    print(q);
+    cout << "Ключ для удаления: " << endl;
+    cin >> r;
+    del_key(q, r);
+    cout << endl;
+    print(q);
+    cout << "Введите элемент перед которым нужно поставить новые: " << endl;
+    cin >> r;
+    cout << "Сколько элементов ввести: " << endl;
+    int count;
+    cin >> count;
+    insert(q, count, r);
+    print(q);
+
+}
+
+void init_queue(queue& q, const char& value) {
+    Node* new_node = new Node;
+    new_node->data = value;
+    q.hn = new_node;
+    q.tn = new_node;
+    q.size = 1;
+}
+void new_queue(queue& q, int n) {
+    char r;
+    cout << "Введите элемент: " << endl;
+    cin >> r;
+    init_queue(q, r);
+    for (int i = 2; i <= n; i++) {
+        cout << "Введите элемент: " << endl;
+        cin >> r;
+        push(q, r);
+    }
+}
+void push(queue& q, const char& value) {
+    Node* new_node = new Node;
+    q.size++;
+    new_node->data = value;
+    new_node->next = nullptr;
+    q.tn->next = new_node;
+    q.tn = new_node;
+}
+void print(queue& q) {
+    Node* tmp = q.hn;
+    while (tmp != nullptr) {
+        cout << tmp->data << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+void pop(queue& q) {
+    Node* tmp = q.hn;
+    q.hn = q.hn->next;
+    q.size--;
+    delete tmp;
+}
+void del_key(queue& q, char r) {
+    int i = 1;
+    while (i <= q.size) {
+        if (q.hn->data == r) {
+            pop(q);
+        }
+        else {
+            push(q, q.hn->data);
+            pop(q);
+            i++;
+        }
+    }
+}
+void insert(queue& q, int count,char r) {
+    Node* cur = new Node;
+    int k = 0;
+    cur = q.hn;
+    cur->data = q.hn->data;
+    while (cur->data != r){
+        push(q, q.hn->data);
+        pop(q);
+        k++;
+    }
+    for(int j = 0;j<count;j++){
+        cout << "Введите элемент добавления: " << endl;
+        cin >> r;
+        push(q, r);
+    }
+    for (k; k <= q.size - count; k++) {
+        push(q, q.hn->data);
+        pop(q);
+    }
+}
---
+++ Labs/11 Лабораторная работа/Lab11.2(Stack)/Lab11.2(Stack).cpp
@@ -0,0 +1,134 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+char r;
+
+struct Stack {
+    char data;
+    Stack* prev;
+};
+Stack* make_stack(int n);
+void print(Stack* first);
+char pop(Stack*& first);
+Stack* push(Stack*& first, char r);
+void download(Stack& stack);
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    int n, k = 0,j = 0;
+    cout << "Количество элементов стека: " << endl;
+    cin >> n;
+    Stack* stack = make_stack(n);
+    print(stack);
+    cout << "Введите ключ удаления: " << endl;
+    cin >> r;
+    Stack* stack2 = make_stack(0);
+    
+    for (int i = 0; i < n; i++) {
+        char del = pop(stack);
+        if (del == r) {
+            k++;
+        }
+        else {
+            push(stack2, del);
+        }
+    }
+    n = n - k;
+    for (int i = 0; i < n; i++) {
+        char del = pop(stack2);
+        push(stack, del);
+    }
+    cout << "Стек после удаления: " << endl;
+    print(stack);
+    cout << "Количество элементов для добавления: " << endl;
+    cin >> k;
+    cout << "Перед каким элементом добавлять: " << endl;
+    cin >> r;
+    while (stack->data != r) {
+        char del = pop(stack);
+        push(stack2, del);
+        j++;
+    }
+    cout << "Введите элементы: " << endl;
+    for (int i = 0; i < k; i++) {
+        cin >> r;
+        push(stack, r);
+    }
+    for (int i = 0; i < n - j + 2;i++) {
+        char del = pop(stack2);
+        push(stack, del);
+    }
+    print(stack);
+    return 0;
+}
+
+Stack* make_stack(int n) {
+    if (n == 0) return 0;
+    Stack* first;
+    Stack* current;
+    first = NULL;
+    current = new Stack;
+    cin >> r;
+    current->data = r;
+    current->prev = NULL;
+    first = current;
+    for (int i = 2; i <= n; i++) {
+        Stack* m = new Stack;
+        cin >> r;
+        m->data = r;
+        m->prev = first;
+        first = m;
+    }
+    return first;
+}
+void print(Stack* first) {
+    if (first == nullptr) cout << "Стек пустой" << endl;
+    Stack* current = first;
+    while (current != NULL) {
+        cout << current->data << " ";
+        current = current->prev;
+    }
+    cout << endl;
+
+}
+char pop(Stack*& first) {
+    Stack* cur = first;
+    int k = 0;
+    while (cur != NULL) {
+        k++;
+        cur = cur->prev;
+    }
+    cur = first;
+    if (k == 1) {
+        char t = first->data;
+        delete cur;
+        first = nullptr;
+        return t;
+    }
+    else {
+        Stack* t = cur->prev;
+        char l = cur->data;
+        first = t;
+        delete cur;
+        return l;
+    }
+}
+Stack* push(Stack*& first, char r) {
+    Stack* cur = new Stack;
+    cur->data = r;
+    cur->prev = first;
+    first = cur;
+    return first;
+}
+void download(Stack*& first) {
+    ofstream out;
+    out.open("File.txt");
+    if (out.is_open()){
+        while (first != nullptr) {
+            out << first->data << endl;
+            first = first->prev;
+        }
+    delete first;
+    }
+}
